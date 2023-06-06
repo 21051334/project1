f96d1be32b689091ba6f1ba92b5b5d7a536d7f01
Sangbartika Saha <122826755+21051334@users.noreply.github.com>
2023-06-06 13:04:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import Notes from "./components/Notes";
+import Footer from "./components/Footer";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
